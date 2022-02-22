f4a4c060a6fbc0a08a01c8a793ca0a15dbf5023c
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-22 10:37:57 +0100

    extracted movement logic to separate class

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -1,20 +1,23 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Spounka.Player
 {
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement
     {
-    #region Variables
-    #endregion
+        private readonly Rigidbody2D _rb;
 
-        private void Start()
+
+        public PlayerMovement(Rigidbody2D rb)
         {
-        
+            _rb = rb;
         }
 
-        private void Update()
+        public void Move(Vector2 input, float movementSpeed, float deltaTime)
         {
-        
+            var dir = input.normalized;
+            var velocity = dir * movementSpeed;
+
+            _rb.MovePosition(_rb.position + (velocity * deltaTime));
         }
     }
 }
