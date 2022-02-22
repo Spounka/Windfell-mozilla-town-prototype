4cf491534fe24b1cd8cfbde9ed9f07942983e64e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-02-22 09:05:24 +0100

    Moved project files to _Project folder

---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spounka
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+    #region Variables
+    #endregion
+
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
