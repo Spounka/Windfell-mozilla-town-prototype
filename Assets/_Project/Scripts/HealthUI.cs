31ca4f278bf439f37ddd4565964506059c2ddb80
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-03-05 15:33:18 +0100

    Assets/_Project/Scripts/HealthSystem.cs

---
+++ Assets/_Project/Scripts/HealthUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(HealthSystem))]
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] private Canvas uiCanvas;
+
+    private HealthSystem _healthSystem;
+    private Slider _healthSlider;
+
+    private void Awake()
+    {
+        _healthSystem = GetComponent<HealthSystem>();
+        uiCanvas ??= GetComponentInChildren<Canvas>();
+
+        if (uiCanvas != null)
+        {
+            _healthSlider = uiCanvas.GetComponentInChildren<Slider>();
+            _healthSlider.value = _healthSlider.maxValue = _healthSystem.MaxHealth;
+        }
+    }
+
+    private void Update()
+    {
+        _healthSlider.value = _healthSystem.CurrentHealth;
+    }
+}
